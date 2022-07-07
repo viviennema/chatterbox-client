7e650445a65b513dda1344e03ff8dee62b1e7ae4
viviennema <98420335+viviennema@users.noreply.github.com>
2022-07-06 20:29:23 -0700

    Work done first night

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -7,7 +7,21 @@ var Parse = {
   server: `https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/${window.CAMPUS}`,
 
   create: function(message, successCB, errorCB = null) {
+    console.log(message);
     // TODO: send a request to the Parse API to save the message
+    $.ajax({
+      url: `https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/${window.CAMPUS}`,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
   },
 
   readAll: function(successCB, errorCB = null) {
